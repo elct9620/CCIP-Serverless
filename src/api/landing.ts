a1b9a371da864abdca926ee41560fbb45b3e53d3
Aotokitsuruya <contact@aotoki.me>
2023-08-16 20:07:29 +0800

    Return 400 error if token not provided to /landing

--- src/api/landing.ts
+++ src/api/landing.ts
@@ -1,5 +1,13 @@
-import { json } from 'itty-router'
+import {
+	json,
+	error,
+	IRequest
+} from 'itty-router'
 
-export function Landing() {
+export function Landing({ query }: IRequest) {
+	if (!query.token) {
+		return error(400, { message: 'invalid token' })
+	}
+
 	return json({nickname: 'Aotoki'})
 }
--- tests/landing.test.ts
+++ tests/landing.test.ts
@@ -3,14 +3,23 @@ import { describe, expect, it, beforeAll, afterAll } from 'vitest';
 
 describe('GET /landing', () => {
 	let server: HttpServer = new HttpServer();
-	beforeAll(() => server.beforeAll());
+	beforeAll(async () => await server.beforeAll());
 
 	it('should return 200 response', async () => {
-		const req = new Request('https://ccip.opass.app/landing', { method: 'GET' });
+		const req = new Request('https://ccip.opass.app/landing?token=dummy', { method: 'GET' });
 		const resp = await server.fetch(req.url);
 		expect(resp.status).toBe(200);
 
 		const json = await resp.json();
 		expect(json).toMatchObject({ nickname: 'Aotoki' });
+	});
+
+	it('should return 400 response', async () => {
+		const req = new Request('https://ccip.opass.app/landing', { method: 'GET' });
+		const resp = await server.fetch(req.url);
+		expect(resp.status).toBe(400);
+
+		const json = await resp.json();
+		expect(json).toMatchObject({ message: 'invalid token' });
 	});
 });
