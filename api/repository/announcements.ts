b6b1bc11a0cddc95e08b42edf020f6441cbf6551
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2023-08-29 16:16:10 +0800

    Add announcement D1 DB repo

---
+++ api/repository/announcements.ts
@@ -0,0 +1,32 @@
+import { type D1Database } from '@cloudflare/workers-types'
+import { Announcement } from '@/announcement'
+
+type AnnouncementSchema = {
+  announced_at: string
+  message_en: string | null
+  message_zh: string | null
+  uri: string
+}
+
+export class D1AnnouncementRepository {
+  private readonly db: D1Database
+
+  constructor(db: D1Database) {
+    this.db = db
+  }
+
+  async findByToken(token?: string): Promise<Announcement[]> {
+    const stmt = this.db.prepare('SELECT * FROM Announcement ORDER BY rowid')
+    const { results } = await stmt.all<AnnouncementSchema>()
+
+    return results.map(toAnnouncement)
+  }
+}
+
+const toAnnouncement = (data: AnnouncementSchema) =>
+  new Announcement({
+    announcedAt: new Date(data.announced_at),
+    messageEn: data.message_en,
+    messageZh: data.message_zh,
+    uri: data.uri,
+  })
