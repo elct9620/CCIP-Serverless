e458eb6750488a5efdace2406fec762137e21075
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2023-08-30 17:12:04 +0800

    Add id column to announcements table

--- migrations/0007_add_announcements.sql
+++ migrations/0007_add_announcements.sql
@@ -1,6 +1,7 @@
 -- Migration number: 0007 	 2023-08-29T06:51:58.079Z
 DROP TABLE IF EXISTS announcements;
 CREATE TABLE IF NOT EXISTS announcements (
+  id INTEGER PRIMARY KEY,
   announced_at DATETIME DEFAULT (datetime('NOW', 'UTC')),
   message_en TEXT,
   message_zh TEXT,
