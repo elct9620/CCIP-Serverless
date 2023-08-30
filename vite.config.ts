1a3f400669ce455c8849929ada522f595cbd9929
Aotokitsuruya <contact@aotoki.me>
2023-08-30 13:55:22 +0800

    Make vite proxy Cloudflare Worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import WorkerProxy from './worker/vite-plugin'
+import routes from './public/_routes.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    WorkerProxy({
+      routes: {
+        include: routes.include,
+        exclude: ['/ui/*', ...routes.exclude],
+      },
+    }),
+  ],
 })
---
+++ worker/vite-plugin.ts
@@ -0,0 +1,142 @@
+import { Plugin } from 'vite'
+import { unstable_dev, UnstableDevWorker } from 'wrangler'
+
+function isRoutingRuleMatch(pathname: string, routingRule: string): boolean {
+  // sanity checks
+  if (!pathname) {
+    throw new Error('Pathname is undefined.')
+  }
+  if (!routingRule) {
+    throw new Error('Routing rule is undefined.')
+  }
+
+  const ruleRegExp = transformRoutingRuleToRegExp(routingRule)
+  return pathname.match(ruleRegExp) !== null
+}
+
+function transformRoutingRuleToRegExp(rule: string): RegExp {
+  let transformedRule
+
+  if (rule === '/' || rule === '/*') {
+    transformedRule = rule
+  } else if (rule.endsWith('/*')) {
+    // make `/*` an optional group so we can match both /foo/* and /foo
+    // /foo/* => /foo(/*)?
+    transformedRule = `${rule.substring(0, rule.length - 2)}(/*)?`
+  } else if (rule.endsWith('/')) {
+    // make `/` an optional group so we can match both /foo/ and /foo
+    // /foo/ => /foo(/)?
+    transformedRule = `${rule.substring(0, rule.length - 1)}(/)?`
+  } else if (rule.endsWith('*')) {
+    transformedRule = rule
+  } else {
+    transformedRule = `${rule}(/)?`
+  }
+
+  // /foo* => /foo.* => ^/foo.*$
+  // /*.* => /*\.* => /.*\..* => ^/.*\..*$
+  transformedRule = `^${transformedRule.replaceAll(/\./g, '\\.').replaceAll(/\*/g, '.*')}$`
+
+  // ^/foo.*$ => /^\/foo.*$/
+  return new RegExp(transformedRule)
+}
+
+const devExclude = ['/@*', '/node_modules/*']
+
+const isMatched = (pathname: string, rules: string[]): boolean =>
+  rules.some(rule => isRoutingRuleMatch(pathname, rule))
+
+interface ProxyRequest {
+  pathname: string
+  method: string
+  headers: Record<string, any>
+  body: Buffer | null
+}
+
+interface ProxyResponse {
+  status: number
+  headers: Record<string, any>
+  body: Buffer
+}
+
+const toArrayBuffer = (buf: Buffer): ArrayBuffer => {
+  const ab = new ArrayBuffer(buf.length)
+  const view = new Uint8Array(ab)
+  for (let i = 0; i < buf.length; ++i) {
+    view[i] = buf[i]
+  }
+  return ab
+}
+
+const proxyTo = async (worker: UnstableDevWorker, req: ProxyRequest): Promise<ProxyResponse> => {
+  const res = await worker.fetch(new URL(`http://vite.localhost${req.pathname}`), {
+    method: req.method,
+    headers: req.headers,
+    body: req.body ? toArrayBuffer(req.body) : null,
+  })
+
+  return {
+    status: res.status,
+    headers: res.headers,
+    body: Buffer.from(await res.arrayBuffer()),
+  }
+}
+
+type ProxyConfig = {
+  routes: {
+    include: string[]
+    exclude: string[]
+  }
+}
+
+export default function WorkerProxy(config: ProxyConfig): Plugin {
+  let worker: UnstableDevWorker | null = null
+
+  if (!config) {
+    throw new Error('Missing routes config')
+  }
+
+  return {
+    name: 'worker-proxy',
+    apply: 'serve',
+    configureServer: async server => {
+      worker = await unstable_dev('worker/index.ts', {})
+
+      server.middlewares.use(async (r, w, next) => {
+        const pathname = r.originalUrl ?? ''
+
+        if (isMatched(pathname, [...devExclude, ...config.routes.exclude])) {
+          return next()
+        }
+
+        if (isMatched(pathname, config.routes.include)) {
+          try {
+            const res = await proxyTo(worker!, {
+              pathname,
+              method: r.method || 'GET',
+              headers: r.headers,
+              body: r.read(),
+            })
+
+            w.writeHead(res.status)
+            for (const [key, value] of Object.entries(res.headers)) {
+              w.setHeader(key, value)
+            }
+
+            w.end(res.body)
+            return
+          } catch (e) {
+            next(e)
+          }
+        }
+
+        next()
+      })
+    },
+    closeBundle: async () => {
+      if (worker) {
+        await worker.stop()
+      }
+    },
+  }
+}
