1df33abcf1549b659663a389a2523d20bad90784
Aotokitsuruya <contact@aotoki.me>
2023-08-31 22:14:29 +0800

    Fix d1 binding name

--- tf/main.tf
+++ tf/main.tf
@@ -1,10 +1,10 @@
 variable "cloudflare_account_id" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "cloudflare_api_token" {
-  type = string
+  type      = string
   sensitive = true
 }
 
@@ -13,14 +13,14 @@ variable "name_suffix" {
 }
 
 variable "d1_id" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 terraform {
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "3.31.0"
     }
   }
@@ -32,18 +32,18 @@ provider "cloudflare" {
 
 resource "cloudflare_workers_kv_namespace" "terraform_state" {
   account_id = var.cloudflare_account_id
-  title = "ccip-terraform-${lower(var.name_suffix)}-state"
+  title      = "ccip-terraform-${lower(var.name_suffix)}-state"
 }
 
 resource "cloudflare_pages_project" "ccip_pages_project" {
-  account_id = var.cloudflare_account_id
+  account_id        = var.cloudflare_account_id
   name              = "ccip-${lower(var.name_suffix)}"
   production_branch = "main"
 
   deployment_configs {
     production {
       d1_databases = {
-        DATABASE = sensitive(var.d1_id)
+        DB = sensitive(var.d1_id)
       }
 
       compatibility_date = "2023-01-09"
