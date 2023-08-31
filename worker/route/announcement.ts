085c0e55ad8469e5de988fdda91d09ea6d1a3397
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2023-08-31 12:22:19 +0800

    Flesh out worker endpoint

--- worker/route/announcement.ts
+++ worker/route/announcement.ts
@@ -1,10 +1,30 @@
 import { IRequest } from 'itty-router'
+import * as schema from '@api/schema'
+import { AnnouncementInfo } from '@api/usecase'
+import { datetimeToUnix } from '@api/utils'
 import { json } from './helper'
 
-export type AnnouncementRequest = IRequest
+export type AnnouncementRequest = {
+  announcementInfo: AnnouncementInfo
+} & IRequest
 
-export type AnnouncementResponse = Object[]
-
-export const announcement = async ({ query }: AnnouncementRequest) => {
-  return json<AnnouncementResponse>([])
+export type AnnouncementData = {
+  datetime: number
+  msgEn: string | null
+  msgZh: string | null
+  uri: string
 }
+
+export type AnnouncementResponse = AnnouncementData[]
+
+export const announcement = async ({ announcementInfo }: AnnouncementRequest) => {
+  const results = await announcementInfo.listAll();
+  return json<AnnouncementResponse>(results.map(toFormattedAnnouncement))
+}
+
+const toFormattedAnnouncement = (data: schema.Announcement): AnnouncementData => ({
+  datetime: datetimeToUnix(data.announcedAt),
+  msgEn: data.messageEn,
+  msgZh: data.messageZh,
+  uri: data.uri,
+})
