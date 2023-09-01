7ad40558996bd88708865c16bba41b07750e6ca1
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2023-09-01 15:27:51 +0800

    Simplify createAnnouncementHandler impl

--- mock/api/announcements.ts
+++ mock/api/announcements.ts
@@ -19,37 +19,12 @@ export const createAnnouncementHandler = async (req: IRequest, { DB }: Env) => {
     'INSERT INTO announcements (id, announced_at, message_en, message_zh, uri, roles) VALUES (?, ?, ?, ?, ?, ?)'
   )
   const payload = await req.json<CreateAnnouncementPayload | CreateAnnouncementPayload[]>()
-
-  let info
-  if (isPayloadInArray(payload)) {
-    info = await DB.batch(
-      payload.map(data =>
-        stmt.bind(
-          data.id,
-          data.announced_at,
-          data.message_en,
-          data.message_zh,
-          data.uri,
-          data.roles
-        )
-      )
+  const payloadArray = Array.isArray(payload) ? payload : [payload]
+  const info = await DB.batch(
+    payloadArray.map(data =>
+      stmt.bind(data.id, data.announced_at, data.message_en, data.message_zh, data.uri, data.roles)
     )
-  } else {
-    info = await stmt
-      .bind(
-        payload.id,
-        payload.announced_at,
-        payload.message_en,
-        payload.message_zh,
-        payload.uri,
-        payload.roles
-      )
-      .run()
-  }
+  )
 
   return json(info)
 }
-
-const isPayloadInArray = (
-  payload: CreateAnnouncementPayload | CreateAnnouncementPayload[]
-): payload is CreateAnnouncementPayload[] => Array.isArray(payload)
