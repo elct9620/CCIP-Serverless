c164766b8220ac43deea3e562e5fb77d41ebe571
Aotokitsuruya <contact@aotoki.me>
2023-09-01 22:02:40 +0800

    Rename worker/route to worker/controller

--- worker/index.ts
+++ worker/index.ts
@@ -2,7 +2,7 @@ import { json, Router, withParams } from 'itty-router'
 import { withUsecases, withTestability } from '@worker/middlewares'
 import { error } from '@worker/utils'
 import { setup } from '@worker/router'
-import * as Controller from '@worker/route'
+import * as Controller from '@worker/controller'
 
 const router = Router()
 
