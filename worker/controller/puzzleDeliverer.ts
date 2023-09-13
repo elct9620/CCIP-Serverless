5946592aa30a72e5af63a4271bbedb7d9eeab6ab
Aotokitsuruya <contact@aotoki.me>
2023-09-13 21:10:14 +0800

    Add Booth List api schema

--- worker/controller/puzzleDeliverer.ts
+++ worker/controller/puzzleDeliverer.ts
@@ -1,9 +1,10 @@
+import * as schema from '@api/schema'
 import { get } from '@worker/router'
 import { json } from '@worker/utils'
 
 export class PuzzleDeliverer {
   @get('/event/puzzle/deliverers')
   async getDeliverers() {
-    return json<string[]>(['COSCUP', 'SITCON'])
+    return json<schema.BoothList>(['COSCUP', 'SITCON'])
   }
 }
