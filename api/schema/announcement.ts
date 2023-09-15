0f36c8929a2076f9c161f2adf4eecabab06b61e5
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2023-09-15 17:28:25 +0800

    Flesh out AnnouncementController#createAnnouncement

--- api/schema/announcement.ts
+++ api/schema/announcement.ts
@@ -4,3 +4,10 @@ export type Announcement = {
   messageZh: string | null
   uri: string
 }
+
+export type CreateAnnouncementParams = {
+  messageEn: string | null
+  messageZh: string | null
+  uri: string
+  roles: string[]
+}
--- worker/controller/announcement.ts
+++ worker/controller/announcement.ts
@@ -19,12 +19,40 @@ export type AnnouncementData = {
 
 export type AnnouncementResponse = AnnouncementData[]
 
+export type AnnouncementPayload = {
+  msg_en: string | null
+  msg_zh: string | null
+  uri: string
+  role: string | string[]
+}
+
 const toFormattedAnnouncement = (data: schema.Announcement): AnnouncementData => ({
   datetime: datetimeToUnix(data.announcedAt),
   msgEn: data.messageEn,
   msgZh: data.messageZh,
   uri: data.uri,
 })
+
+const toCreateAnnouncementParams = (data: AnnouncementPayload): schema.CreateAnnouncementParams => {
+  let roles: string[]
+  try {
+    if (Array.isArray(data.role)) {
+      roles = data.role.map(String)
+    } else {
+      const parsed = JSON.parse(data.role)
+      roles = Array.isArray(parsed) ? parsed.map(String) : []
+    }
+  } catch {
+    roles = []
+  }
+
+  return {
+    messageEn: typeof data.msg_en === 'string' ? data.msg_en : null,
+    messageZh: typeof data.msg_zh === 'string' ? data.msg_zh : null,
+    uri: typeof data.uri === 'string' ? data.uri : '',
+    roles
+  }
+}
 
 export class AnnouncementController {
   @get('/announcement')
@@ -35,8 +63,8 @@ export class AnnouncementController {
 
   @post('/announcement')
   async createAnnouncement(request: AnnouncementRequest) {
-    const params = await new Response(request.body).json<Record<string, unknown>>()
-    await request.announcementInfo.create(params)
+    const params = await new Response(request.body).json<AnnouncementPayload>()
+    await request.announcementInfo.create(toCreateAnnouncementParams(params))
     return json({ status: 'OK' })
   }
 }
