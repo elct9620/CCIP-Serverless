31a9a584027f3586a2d3581dbb5a2261c8e66c98
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2023-09-27 12:08:28 +0800

    Remove AnnouncementInfo#byAttendee

--- api/command/announcementInfo.ts
+++ api/command/announcementInfo.ts
@@ -1,28 +1,13 @@
-import { Attendee, AttendeeRole } from '@/attendee'
-import { Repository } from '@/core'
 import { Announcement } from '@api/schema'
 import { AnnouncementRepository } from './repository'
 
 export type AnnouncementReply = Announcement[]
 
-const defaultQueryRole = AttendeeRole.Audience
-
 export class AnnouncementInfo {
   private readonly announcementRepository: AnnouncementRepository
-  private readonly attendeeRepository: Repository<Attendee>
 
-  constructor(
-    announcementRepository: AnnouncementRepository,
-    attendeeRepository: Repository<Attendee>
-  ) {
+  constructor(announcementRepository: AnnouncementRepository) {
     this.announcementRepository = announcementRepository
-    this.attendeeRepository = attendeeRepository
-  }
-
-  public async byAttendee(token?: string | undefined): Promise<AnnouncementReply> {
-    const attendee = await this.attendeeRepository.findById(token ?? '')
-    const results = await this.announcementRepository.listByRole(attendee?.role ?? defaultQueryRole)
-    return results.map(toAnnouncementData)
   }
 
   public async create(_params: Record<string, unknown>): Promise<void> {
@@ -37,15 +22,3 @@ export class AnnouncementInfo {
     await this.announcementRepository.create(fixedValue)
   }
 }
-
-const toAnnouncementData = (data: {
-  announcedAt: Date
-  messageEn: string | null
-  messageZh: string | null
-  uri: string
-}): Announcement => ({
-  announcedAt: data.announcedAt,
-  messageEn: data.messageEn,
-  messageZh: data.messageZh,
-  uri: data.uri,
-})
