310206971faaa72f98e5e9178e52e3a0e1387791
Aotokitsuruya <contact@aotoki.me>
2023-11-23 19:51:15 +0800

    Refactor landign API to OpenAPI route

--- worker/controller/landing.ts
+++ worker/controller/landing.ts
@@ -2,7 +2,8 @@ import { IRequest, StatusError } from 'itty-router'
 import { json } from '@worker/utils'
 import * as schema from '@api/schema'
 import { AttendeeInfo } from '@api/query'
-import { get } from '@worker/router'
+import { Get } from '@worker/router'
+import { OpenAPIRoute, OpenAPIRouteSchema } from '@cloudflare/itty-router-openapi'
 
 export type LandingRequest = {
   attendeeInfo: AttendeeInfo
@@ -10,9 +11,17 @@ export type LandingRequest = {
 
 export type LandingResponse = schema.BasicAttendeeInfo
 
-export class LandingController {
-  @get('/landing')
-  async landing({ attendeeInfo, query }: LandingRequest) {
+@Get('/landing')
+export class Landing extends OpenAPIRoute {
+  static schema: OpenAPIRouteSchema = {
+    summary: 'Get attendee display name',
+    tags: ['Attendee'],
+    parameters: {
+      token: schema.OptionalTokenQuery,
+    },
+  }
+
+  async handle({ attendeeInfo, query }: LandingRequest) {
     if (!query.token) {
       throw new StatusError(400, 'token required')
     }
