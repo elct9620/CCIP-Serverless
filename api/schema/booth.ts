72e808c7883871fc65c7f23a8258fa83bb6f6889
Mikucat <admin@mikucat.me>
2023-11-24 17:38:20 +0800

    Refactor booth schema with zod

--- api/schema/booth.ts
+++ api/schema/booth.ts
@@ -1,4 +1,9 @@
-export type BoothList = string[]
-export type BoothStaff = {
-  slug: string
-}
+import { z } from "zod"
+
+export type BoothList = z.infer<typeof boothListSchema>
+export const boothListSchema = z.array(z.string())
+
+export type BoothStaff = z.infer<typeof boothStaffSchema>
+export const boothStaffSchema = z.object({
+  slug: z.string()
+})
