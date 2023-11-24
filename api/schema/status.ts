1fe723a707a9be371bfb273c3e4cefda17418ff7
Mikucat <admin@mikucat.me>
2023-11-24 17:39:16 +0800

    Refactor status schema with zod

--- api/schema/status.ts
+++ api/schema/status.ts
@@ -1,12 +1,14 @@
-import { Scenario } from './scenario'
-import { AttendeeMetadata } from './attendee'
+import { scenarioSchema } from './scenario'
+import { attendeeMetadataSchema } from './attendee'
+import { z } from 'zod'
 
-export type Status = {
-  event_id: string
-  public_token: string
-  user_id: string
-  first_use: number | null
-  role: string
-  scenario: Record<string, Scenario>
-  attr: AttendeeMetadata
-}
+export type Status = z.infer<typeof statusSchema>
+export const statusSchema = z.object({
+  event_id: z.string().default('COSCUP_2023'),
+  public_token: z.string().default('8b1619a7-347c-477a-9045-e48a7828a235'),
+  user_id: z.string().default('user1234'),
+  first_use: z.number().nullable().describe('timestamp').default(1650000000),
+  role: z.string().default('audience'),
+  scenario: z.record(scenarioSchema).describe('Scenario ID => Scenario'),
+  attr: attendeeMetadataSchema,
+})
