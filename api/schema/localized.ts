a52d43f01186c35bf5050c4a882697a5cc1fd0cd
Mikucat <admin@mikucat.me>
2023-11-24 17:38:36 +0800

    Refactor localized schema with zod

--- api/schema/localized.ts
+++ api/schema/localized.ts
@@ -1,6 +1,12 @@
+import { z } from 'zod'
+
 export enum Languages {
   zhTW = 'zh-TW',
   enUS = 'en-US',
 }
 
-export type LocalizedText = Partial<Record<Languages, string>>
+export type LocalizedText = z.infer<typeof localizedTextSchema>
+export const localizedTextSchema = z
+  .record(z.nativeEnum(Languages), z.string())
+  .describe('RFC5646 Language Tag => Text')
+  .default({ 'en-US': 'Check-in' })
