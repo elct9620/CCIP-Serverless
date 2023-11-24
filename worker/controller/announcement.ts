8d42ae824ae05b62a5a95c772653e8f49360e758
Mikucat <admin@mikucat.me>
2023-11-24 17:43:09 +0800

    Add announcement response schema

--- worker/controller/announcement.ts
+++ worker/controller/announcement.ts
@@ -6,6 +6,7 @@ import * as Command from '@api/command'
 import { ListAnnouncementsByToken } from '@api/query'
 import { datetimeToUnix } from '@api/utils'
 import { Get, Post } from '@worker/router'
+import { z } from 'zod'
 
 export type AnnouncementRequest = {
   listAnnouncementsByToken: ListAnnouncementsByToken
@@ -13,12 +14,14 @@ export type AnnouncementRequest = {
   query: Record<string, string | undefined>
 } & IRequest
 
-export type AnnouncementData = {
-  datetime: number
-  msgEn: string | null
-  msgZh: string | null
-  uri: string
-}
+export type AnnouncementData = z.infer<typeof announcementDataSchema>
+
+export const announcementDataSchema = z.object({
+  datetime: z.number().default(1600000000),
+  msgEn: z.string().nullable().default('english'),
+  msgZh: z.string().nullable().default('中文'),
+  uri: z.string().default('https://example.com/'),
+})
 
 const toCreateAnnouncementParams = (
   data: schema.CreateAnnouncementPayload
@@ -33,7 +36,8 @@ const toCreateAnnouncementParams = (
   }
 }
 
-export type AnnouncementResponse = AnnouncementData[]
+export type AnnouncementResponse = z.infer<typeof announcementResponseSchema>
+export const announcementResponseSchema = z.array(announcementDataSchema)
 
 const toFormattedAnnouncement = (data: schema.Announcement): AnnouncementData => ({
   datetime: datetimeToUnix(data.announcedAt),
@@ -50,6 +54,12 @@ export class ListAnnouncement extends OpenAPIRoute {
     parameters: {
       token: schema.OptionalAttendeeTokenQuery,
     },
+    responses: {
+      '200': {
+        description: 'Returns list of announcements',
+        schema: announcementResponseSchema,
+      },
+    },
   }
 
   async handle(request: AnnouncementRequest) {
@@ -65,6 +75,12 @@ export class CreateAnnouncement extends OpenAPIRoute {
   static schema: OpenAPIRouteSchema = {
     summary: 'Create an announcement',
     tags: ['Announcement'],
+    responses: {
+      '200': {
+        description: 'Created an announcement',
+        schema: { status: 'OK' },
+      },
+    },
   }
 
   async handle(request: AnnouncementRequest) {
