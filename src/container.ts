51ab5aaf3a042d364d7de2f76f1832e865790dc6
Aotokitsuruya <contact@aotoki.me>
2024-04-09 21:31:40 +0800

    Remove useless inversify decorator

--- src/container.ts
+++ src/container.ts
@@ -1,9 +1,6 @@
 import 'reflect-metadata'
 import { Container } from 'inversify'
-import getDecorators from 'inversify-inject-decorators'
 
 const container = new Container()
 
-// NOTE: the default export of getDecorators is a function in this environment
-export const { lazyInject } = getDecorators.default(container)
 export default container
